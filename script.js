6b7315b9702ac638bccda7b7e858d2c92e132497
Rishikesh_Sonawane <rishikeshsonawane1465@gmail.com>
2021-07-29 11:28:24 +0530

    image changes

---
+++ script.js
@@ -0,0 +1,30 @@
+var settingmenu = document.querySelector(".settings");
+
+function settingMenuToggle(){
+    settingmenu.classList.toggle("settings-height");
+}
+
+var darkBtn = document.getElementById("dark-btn");
+
+darkBtn.onclick = function(){
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+
+    if(localStorage.getItem("theme") == "light")
+        localStorage.setItem("theme", "dark");
+    else
+        localStorage.setItem("theme", "light");
+}
+
+
+if(localStorage.getItem("theme") == "light"){
+    darkBtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+}
+else if(localStorage.getItem("theme") == "dark"){
+    darkBtn.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme");
+}
+else{
+    localStorage.setItem("theme", "light");
+}
